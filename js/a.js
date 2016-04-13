6a4ce20521ba22ea765edfaae5ba480769801aa1
OA Wu <comdan66@gmail.com>
2016-04-13 11:46:26 +0800

    Finish!

--- js/a.js
+++ js/a.js
@@ -3,6 +3,7 @@
  * @copyright   Copyright (c) 2016 OA Wu Design
  */
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-46121102-24', 'auto');ga('send', 'pageview');
 window.fbAsyncInit = function() {FB.init({appId: '695906407210191',xfbml: true,version: 'v2.4'});};
 (function(d, s, id){var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) {return;}js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/zh_TW/sdk.js";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));
 
